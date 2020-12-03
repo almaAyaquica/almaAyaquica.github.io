946b0100fb07f799681b1675470c0b15b1fbe541
Alma Ayaquica <yogo060@gmail.com>
2020-12-03 14:13:08 -0600

    scroll test

--- js/main.js
+++ js/main.js
@@ -11,7 +11,7 @@ $(window).on('load', function() {
 
 });
 if ($(window).width() > 992) {
-	$(window).scroll(function(){  console.log($(window).scrollTop());
+	$(window).scroll(function(){ 
 	   if ($(this).scrollTop() > 40) {
 		  $('#navbar_top').addClass("fixed-top");
 		  // add padding top to show content behind navbar
@@ -34,6 +34,30 @@ if ($(window).width() > 992) {
         $(e.target).find('.dropdown-menu').toggle();
         
     });
+});
+//scroll v2.0
+$(document).ready(function(){
+	// Add smooth scrolling to all links
+	$("a").on('click', function(event) {
+		// Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
 });
 
 (function($) {
