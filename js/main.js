af968b5cf91f78d261b693e8c2b6d745a6508ea2
Alma Ayaquica <yogo060@gmail.com>
2020-12-22 22:02:08 -0600

    scroll & menu

--- js/main.js
+++ js/main.js
@@ -10,6 +10,8 @@ $(window).on('load', function() {
 	$("#preloder").delay(400).fadeOut("slow");
 
 });
+
+//scroll
 if ($(window).width() > 992) {
 	$(window).scroll(function(){ 
 	   if ($(this).scrollTop() > 40) {
@@ -28,38 +30,28 @@ if ($(window).width() > 992) {
 		}   
 	});
   } // end if
-  
-  $(function () {
-    $('.dropdown-menu a').click(function (e) {
-        $(e.target).find('.dropdown-menu').toggle();
-        
+
+//scroll btn
+$(document).ready(function () {
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 40) {
+            $('.scrollup').fadeIn();
+           
+        } else {
+            $('.scrollup').fadeOut();
+        }
     });
+
+    $('.scrollup').click(function () {
+        $("html, body").animate({
+            scrollTop: 0
+        }, 600);
+        return false;
+    });
+
 });
-//scroll v2.0
-/*
-$(document).ready(function(){
-	// Add smooth scrolling to all links
-	$("a").on('click', function(event) {
-		// Make sure this.hash has a value before overriding default behavior
-    if (this.hash !== "") {
-      // Prevent default anchor click behavior
-      event.preventDefault();
 
-      // Store hash
-      var hash = this.hash;
-
-      // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 800, function(){
-
-        // Add hash (#) to URL when done scrolling (default click behavior)
-        window.location.hash = hash;
-      });
-    } // End if
-  });
-});*/
 
 (function($) {
 
